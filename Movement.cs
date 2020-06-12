36a17000bf797ae2a0fdf25aa3b259af422ca494
thegreatab <49879312+thegreatab@users.noreply.github.com>
2020-06-12 14:30:36 -0400

    Original player code
    Very very very unfinished!

---
+++ Movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float speed;
+	public static Vector3 movement;
+	public Rigidbody rigidity;
+
+	void Start () {
+		speed = 2.0f;
+		movement = new Vector3(0.0f,0.0f,-75f);
+		rigidity = GetComponent<Rigidbody>();
+	}
+	
+
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+		movement.x += speed * moveHorizontal;
+		movement.z += speed * moveVertical;
+		rigidity.position = movement;
+	}
+}
